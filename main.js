515e76955b7070984ad6024e7f91db8eef48af2a
MiguelD-PC <mikdominguez@gmail.com>
2022-05-02 00:42:53 +0200

    JS Calendar numbers

--- main.js
+++ main.js
@@ -1,3 +1,61 @@
+//CALENDAR VARIABLES
+const date = new Date();
+const weekday = date.getDay();
+const day = date.getDate();
+const month = date.getMonth();
+
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+let monthDays = document.querySelector(".days");
+let prevMonthDays = document.querySelector(".prev-date");
+let nextMonthDays = document.querySelector(".next-date");
+
+//EVENT SECTION HEADER
+document.querySelector(".titleWeekdaySection").innerHTML = weekdays[weekday];
+document.querySelector(".titleMonthEventSection").innerHTML = months[month] + " " + day;
+
+const lastDayoftheMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+const lastMonthDays = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+const upcomingMonthDays = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+
+const lastWeekday = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay();
+console.log(weekdays[lastWeekday]);
+
+// CALENDAR NUMBERS
+let days = "";
+let prevDate = "";
+
+function getFirstWeekday() {
+    date.setDate(1);
+    return date.getDay();
+}
+
+function renderCalendar() {
+    for (let i = 1; i <= lastDayoftheMonth; i++) {
+        days += `<div>${i}</div>`
+        monthDays.innerHTML = days;
+    }
+
+    for (let i = getFirstWeekday(); i > 0; i--) {
+        prevDate += `<div class="prev-date">${lastMonthDays - i}</div>`
+        prevMonthDays.innerHTML = prevDate;
+    }
+
+    for (let i = 1; i > (6 - lastWeekday); i++) {
+        nextDate += `<div class="next-date">${i}</div>`
+        nextMonthDays.innerHTML = nextDate;
+    }
+}
+
+
+renderCalendar();
+
+
+
+
+//MODAL SECTION
+
 // Get the modal
 var modal = document.getElementById("eventModal");
 
@@ -23,5 +81,3 @@ window.onclick = function (event) {
         modal.style.display = "none";
     }
 }
-
-// CALENDAR NUMBER
